d70be4cb74e1cc473b9d2099cc330ca2259f965f
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-07-17 21:36:36 +0200

    create a new project with terraform

---
+++ terraform/project_k8s.tf
@@ -0,0 +1,7 @@
+# Create a new project
+resource "openstack_identity_project_v3" "k8s_cluster" {
+	name				= "k8s-cluster"
+	description	= "Creating Kubernetes cluster"
+	is_domain		= false
+	enabled			= true
+}
