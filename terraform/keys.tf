e2656fc965bcde3b36750f7e70036b8aebbf2bd8
sergii-moroz <70237603+sergii-moroz@users.noreply.github.com>
2025-07-18 10:58:22 +0200

    Update keys.tf

--- terraform/keys.tf
+++ terraform/keys.tf
@@ -1,11 +1,11 @@
-# resource "openstack_compute_keypair_v2" "k8s_key" {
-# 	provider	= openstack.user
-# 	name			= "k8s-key"
-# }
+resource "openstack_compute_keypair_v2" "k8s_key" {
+	provider	= openstack.user
+	name			= "k8s-key"
+}
 
-# resource "local_file" "k8s_key_pem" {
-# 	provider				= openstack.user
-# 	content					= openstack_compute_keypair_v2.k8s_key.private_key
-# 	filename				= "k8s-key.pem"
-# 	file_permission	= "0600"
-# }
+resource "local_file" "k8s_key_pem" {
+	provider				= local
+	content					= openstack_compute_keypair_v2.k8s_key.private_key
+	filename				= "k8s-key.pem"
+	file_permission	= "0600"
+}
