0f9825d229c630e797747dd1d00b790287a44dd6
sergii-moroz <70237603+sergii-moroz@users.noreply.github.com>
2025-07-18 10:58:40 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
 			source = "terraform-provider-openstack/openstack"
 			version = "~> 1.52.1"
 		}
+		local = {
+			source = "hashicorp/local"
+      		version = "~> 2.4"
+		}
 	}
 }
 
